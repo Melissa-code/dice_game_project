9c2fb6f5d970a32fd047bd2332ff1bf6284c9fa3
Melissa Benard <benardmeli@yahoo.fr>
2022-02-21 18:00:52 +0100

    method roll the dice

--- main.js
+++ main.js
@@ -40,6 +40,16 @@ class Player {
         this.roundScore = roundScore; 
         this.globalScore = globalScore;
     }
+
+    
+    /***********************  roll the dice ************************ */
+
+    rollThedice(diceResult) {
+        diceResult = this.dice.numbersDice;
+        diceRolling = diceResult[Math.floor(Math.random() * diceResult.length)];
+        console.log(diceRolling); 
+        return diceRolling;
+    }
 }
 
 
@@ -57,9 +67,4 @@ let player1 = new Player('Player 1', dice,  0, 0);
 let player2 = new Player('Player 2', dice, 0, 0); 
 let game = new Game([player1, player2]); 
 
-
-
-console.log(dice);
-console.log(player1);
-console.log(player2);
-console.log(game);
+player1.rollThedice(numbersDice);
